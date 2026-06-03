383219ffc4b78e811cb08e27285ad15eb8e5769b
blackcode2301 <r2163306@gmail.com>
2026-06-03 08:38:38 -0300

    bla

--- Main.java
+++ Main.java
@@ -1,23 +1,8 @@
+
 public class Main {
-
-    static void primo(int number) {
-
-        int[] array = new int[number];
-
-        for (int i = 0; i < number; i++) {
-            array[i] = number - i;
-
-        }
-        for (int i = 0; i < array.length; i++) {
-            if (number % array[i] == 0) {
-
-            }
-        }
-    }
-
     public static void main(String[] args) {
 
-        primo(7);
-    }
+        
 
+    }
 }
