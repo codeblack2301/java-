4efd5803c1f12d3a722a21d64bd9e427b90fdf96
blackcode2301 <r2163306@gmail.com>
2026-06-08 15:56:41 -0300

    estudo das threads

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Car car = new Car("cahrger", "yellow", 20000);
+        System.out.println("you have 5 seconds to enter your name ");
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
 
-        car.setColor("blue");
-        System.out.println(car.getColor());
+        thread.start();
+
+        System.out.println("enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("hello " + name);
 
     }
 
